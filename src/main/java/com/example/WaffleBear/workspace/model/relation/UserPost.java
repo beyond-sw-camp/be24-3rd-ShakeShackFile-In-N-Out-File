fcdc92ff7de6b170415caffbcaa8de6f0d7a2ac7
Lumisia <zh2171@gmail.com>
2026-02-27 18:15:35 +0900

    [Refactor] Posts => workspace로 수정

---
+++ src/main/java/com/example/WaffleBear/workspace/model/relation/UserPost.java
@@ -0,0 +1,25 @@
+package com.example.WaffleBear.workspace.model.relation;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Entity
+public class UserPost {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idx;
+
+
+}
