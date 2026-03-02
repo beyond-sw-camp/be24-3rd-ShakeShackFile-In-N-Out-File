fc83a9bff4e77d1d594b36078f94a104047c0b06
chefbeom <h002374@naver.com>
2026-03-02 14:53:29 +0900

    [Feat] - 파일 업로드 기능 및 인터페이스 교체 가능

---
+++ src/main/java/com/example/WaffleBear/file/service/FileUpDownloadService.java
@@ -0,0 +1,13 @@
+package com.example.WaffleBear.file.service;
+
+import com.example.WaffleBear.file.model.FileInfoDto;
+
+import java.util.List;
+
+public interface FileUpDownloadService {
+    List<FileInfoDto.FileRes> fileUpload(List<FileInfoDto.FileReq> requests);
+
+    FileInfoDto.FileRes fileDownload(FileInfoDto.FileReq dto);
+
+    FileInfoDto.FileRes fileList(Long idx);
+}
