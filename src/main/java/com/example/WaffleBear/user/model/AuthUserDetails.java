2aea4535fb878e8ca00ff08add1a0574db685a15
lee <sunyeoplee0@gmail.com>
2026-03-03 16:12:15 +0900

    [Feat] 소셜 로그인(카카오,네이버,구글) 기능 구현

--- src/main/java/com/example/WaffleBear/user/model/AuthUserDetails.java
+++ src/main/java/com/example/WaffleBear/user/model/AuthUserDetails.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 @Builder
-public class AuthUserDetails implements UserDetails {
+public class AuthUserDetails implements UserDetails, OAuth2User {
     @Getter
     private Long idx;
     @Getter
@@ -19,6 +21,8 @@ public class AuthUserDetails implements UserDetails {
     private Boolean enable;
     @Getter
     private String role;
+    private String name;
+    private Map<String, Object> attributes;
 
     public static AuthUserDetails from(User entity) {
         return AuthUserDetails.builder()
@@ -40,6 +44,16 @@ public class AuthUserDetails implements UserDetails {
 
     public String getRole() {
         return role;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public String getName() {
+        return name;
     }
 
     @Override
@@ -76,4 +90,6 @@ public class AuthUserDetails implements UserDetails {
     public boolean isEnabled() {
         return enable;
     }
+
+
 }
