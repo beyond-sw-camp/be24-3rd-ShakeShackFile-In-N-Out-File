272f864932a9a823c30e0e2da3cbece87aa204d0
lee <sunyeoplee0@gmail.com>
2026-03-11 16:19:01 +0900

    [Fix] 로그인 기능 수정

--- src/main/java/com/example/WaffleBear/user/controller/AuthController.java
+++ src/main/java/com/example/WaffleBear/user/controller/AuthController.java
@@ -7,10 +7,12 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = {"http://localhost:5173", "http://127.0.0.1:5173"}, allowCredentials = "true")
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
