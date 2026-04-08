d5281e0889ae437d5e03e53f4cb57af4a938e4b1
Lumisia <zh2171@gmail.com>
2026-04-08 09:10:27 +0900

    [Test] TestController 추가함

---
+++ src/main/java/com/example/WaffleBear/TestController.java
@@ -0,0 +1,13 @@
+package com.example.WaffleBear;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/version")
+    public void Test() {
+        System.out.println("v1");
+    }
+}
